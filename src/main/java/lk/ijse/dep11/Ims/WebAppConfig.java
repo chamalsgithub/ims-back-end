29be5da8d83e2e3405f302d5c9557c3bcad5791d
chamal995 <chamal995@gmail.com>
2023-11-22 14:30:04 +0530

    initial commit

---
+++ src/main/java/lk/ijse/dep11/Ims/WebAppConfig.java
@@ -0,0 +1,12 @@
+package lk.ijse.dep11.Ims;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan
+@EnableWebMvc
+public class WebAppConfig {
+}
---
+++ src/main/java/lk/ijse/dep11/Ims/WebRootConfig.java
@@ -0,0 +1,7 @@
+package lk.ijse.dep11.Ims;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebRootConfig {
+}
